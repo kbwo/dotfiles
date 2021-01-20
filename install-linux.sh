81f6d33e27a9a872cf864db956f33883c29d908d
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-01-20 20:03:33 +0900

    remove some commands

--- install-linux.sh
+++ install-linux.sh
@@ -2,9 +2,6 @@
 if type "apt-get" > /dev/null 2>&1; then
   apt-get update
   apt-get install -y libncurses5-dev silversearcher-ag
-  apt-get install neovim
-  apt-get install python-neovim
-  apt-get install python3-neovim
   apt-get install zsh
 fi
 
