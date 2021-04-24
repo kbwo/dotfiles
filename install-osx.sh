5118f27c6597e16f501fd7a35f0b16b423080b16
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-04-24 19:49:29 +0900

    update install osx script

--- install-osx.sh
+++ install-osx.sh
@@ -13,6 +13,14 @@ brew install ghq
 brew install fzf
 brew install ripgrep
 brew install pyenv
+brew install neovim
+brew install alacritty
+brew install node
+brew install node
+brew tap sanemat/font
+brew install ricty
+cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
+fc-cache -vf
 fisher install jethrokuan/z
 fisher install oh-my-fish/theme-bobthefish
 fisher install decors/fish-ghq
@@ -27,3 +35,5 @@ pyenv install 3.7.0
 pyenv global 3.7.0
 sh ~/dotfiles/install-nvim.sh
 pip3 install pynvim
+npm install -g n
+n 14.5.0
