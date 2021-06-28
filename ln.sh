498faa0dce265ea266a4f4fdd1b4a2bea034c87c
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-06-28 13:06:35 +0900

    add p10k ln script

--- ln.sh
+++ ln.sh
@@ -4,6 +4,7 @@ mkdir -p ~/.config/fish
 ln -sf ~/dotfiles/.config/nvim/init.vim ~/.vimrc
 ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
 ln -sf ~/dotfiles/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -sf ~/dotfiles/.p10k.zsh !/.p10k.zsh
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.config/fish/config.fish ~/.config/fish/config.fish
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
