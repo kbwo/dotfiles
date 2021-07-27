f70c70f1ba83449ab99dd22bbaf37f7090a64d95
Kodai Kabasawa <kabaaa1126@gmail.com>
2021-07-28 00:07:20 +0900

    remove fish install script

--- install-linux.sh
+++ install-linux.sh
@@ -3,13 +3,7 @@ if type "apt-get" > /dev/null 2>&1; then
   apt-get update
   apt-get install -y libncurses5-dev silversearcher-ag
   apt-get install zsh
-  apt-get install fish
-  curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-  apt-get install exa
   apt-get install ghq
-  fisher install jethrokuan/z
-  fisher install oh-my-fish/theme-bobthefish
-  fisher install decors/fish-ghq
 fi
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
--- install-python.sh
+++ install-python.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 cd
-wget https://www.python.org/ftp/python/3.7.3/Python-3.7.3.tar.xz
-tar xf Python-3.7.3.tar.xz
-cd ./Python-3.7.3
+wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tar.xz
+tar xf Python-3.9.1.tar.xz
+cd ./Python-3.9.1
 ./configure
 make
 make install
-update-alternatives --install /usr/bin/python3 python3 /usr/local/bin/python3.7 10
+update-alternatives --install /usr/bin/python3 python3 /usr/local/bin/python3.9 10
 pip3 install pynvim
