da6504d88e8d6cb02d9a169e1a59043bc87de4b9
kbwo <kabaaa1126@gmail.com>
2022-05-12 11:38:13 +0900

    add zsh-abbr

--- .zsh.d/zinit.zsh
+++ .zsh.d/zinit.zsh
@@ -14,6 +14,7 @@ autoload -Uz _zinit
 ZDOTDIR="${HOME}/dotfiles/.zsh.d"
 zinit light sindresorhus/pure
 zinit light agkozak/zsh-z
+zinit light olets/zsh-abbr
 autoload -Uz compinit && compinit
 zinit wait lucid light-mode as'null' \
     atinit'. "$ZDOTDIR/zinit/plugins.zsh"' \
