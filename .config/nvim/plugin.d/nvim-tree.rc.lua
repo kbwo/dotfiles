08316c1421284db88ac0a257c83dfd62d0670544
kbwo <kabaaa1126@gmail.com>
2023-01-25 11:16:51 +0900

    update

--- .config/nvim/plugin.d/nvim-tree.rc.lua
+++ .config/nvim/plugin.d/nvim-tree.rc.lua
@@ -2,6 +2,10 @@ local HEIGHT_RATIO = 0.8  -- You can change this
 local WIDTH_RATIO = 0.5   -- You can change this too
 
 require('nvim-tree').setup({
+  git = {
+    enable = false,
+    ignore = false
+  },
   renderer = {
     icons = {
       show = {
