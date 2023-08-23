8e557dba701aa5ffdd64a0b5411b706c255b473d
kbwo <kabaaa1126@gmail.com>
2023-08-23 23:08:41 +0900

    update

---
+++ .config/nvim/plugin.d/comment.rc.lua
@@ -0,0 +1,14 @@
+require('Comment').setup({
+  toggler = {
+    ---Line-comment toggle keymap
+    line = 'gal',
+    ---Block-comment toggle keymap
+    block = 'gab',
+  },
+  opleader = {
+      ---Line-comment keymap
+      line = 'gal',
+      ---Block-comment keymap
+      block = 'gab',
+  },
+})
