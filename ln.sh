2e40322519d75a828774418af809dec03277cba8
Kodai Kabasawa <kabaaa1126@gmail.com>
2023-10-17 22:48:15 +0900

    udpate

--- ln.sh
+++ ln.sh
@@ -2,8 +2,6 @@
 mkdir -p ~/.config/nvim
 # ln -sf ~/dotfiles/.config/nvim/init.vim ~/.vimrc
 ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/dotfiles/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.alacritty.arch.yml ~/.alacritty.yml
-ln -sf ~/dotfiles/ultisnips/* ~/.config/coc/ultisnips
