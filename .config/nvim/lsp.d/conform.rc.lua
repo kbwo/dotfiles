38ff69222281c9cc82f6a78e928a17b32250cbd4
Kodai Kabasawa <kabaaa1126@gmail.com>
2023-12-16 16:49:46 +0900

    update

--- .config/nvim/lsp.d/conform.rc.lua
+++ .config/nvim/lsp.d/conform.rc.lua
@@ -1,63 +1,53 @@
+local util = require("conform.util")
 local rustfmt = require("conform.formatters.rustfmt")
 rustfmt.args = function()
-	return { "--edition", "2021", "--emit", "stdout" }
+  return { "--edition", "2021", "--emit", "stdout" }
+end
+local eslint_d = require("conform.formatters.eslint_d")
+-- eslint_d.command = util.from_node_modules("eslint")
+eslint_d.cwd = util.root_file({
+  ".eslintrc",
+  ".eslintrc.js",
+  ".eslintrc.json",
+  "eslintrc.js",
+  "eslintrc.json",
+})
+
+local is_available = function(name, bufnr)
+  return require("conform").get_formatter_info(name, bufnr).available
 end
 
-local is_available = function(name)
-	return require("conform").get_formatter_info(name, bufnr).available
-end
-
-local prettier_or_lsp = function(bufnr)
-	if is_available("prettierd") or is_available("prettier") then
-		return { "prettierd", "prettier" }
-	else
-		return { "lsp" }
-	end
-end
-
-local stylua_or_lsp = function(bufnr)
-	if is_available("stylua") then
-		return { "stylua" }
-	else
-		return { "lsp" }
-	end
-end
-
-local cs_fixer_or_lsp = function(bufnr)
-	if is_available("php_cs_fixer") then
-		return { "php_cs_fixer" }
-	else
-		return { "lsp" }
-	end
-end
+local eslint_or_prettier_or_lsp = { "eslint_d", "prettierd", "prettier", "lsp" }
+local stylua_or_lsp = { "stylua", "lsp" }
 
 require("conform").setup({
-	formatters_by_ft = {
-		javascript = prettier_or_lsp,
-		typescriptreact = prettier_or_lsp,
-		typescript = prettier_or_lsp,
-		typescript = prettier_or_lsp,
-		json = prettier_or_lsp,
-		css = prettier_or_lsp,
-		html = prettier_or_lsp,
-		lua = stylua_or_lsp,
-		rust = { "rustfmt" },
-	},
-	format_on_save = {
-		-- I recommend these options. See :help conform.format for details.
-		lsp_fallback = true,
-		timeout_ms = 500,
-	},
+  log_level = vim.log.levels.DEBUG,
+  formatters_by_ft = {
+    javascript = eslint_or_prettier_or_lsp,
+    typescriptreact = eslint_or_prettier_or_lsp,
+    typescript = eslint_or_prettier_or_lsp,
+    json = eslint_or_prettier_or_lsp,
+    css = eslint_or_prettier_or_lsp,
+    html = eslint_or_prettier_or_lsp,
+    lua = { "stylua", "lsp" },
+    php = { "csharpier", "lsp" },
+    rust = { "rustfmt" },
+  },
+  format_on_save = {
+    -- I recommend these options. See :help conform.format for details.
+    lsp_fallback = true,
+    timeout_ms = 500,
+  },
 })
 
 vim.api.nvim_create_user_command("Format", function(args)
-	local range = nil
-	if args.count ~= -1 then
-		local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-		range = {
-			start = { args.line1, 0 },
-			["end"] = { args.line2, end_line:len() },
-		}
-	end
-	require("conform").format({ async = true, lsp_fallback = true, range = range })
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ["end"] = { args.line2, end_line:len() },
+    }
+  end
+  require("conform").format({ async = true, lsp_fallback = true, range = range })
 end, { range = true })
