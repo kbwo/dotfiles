684f571ec1419f606fccd0d0521828542a294d37
kbwo <kabaaa1126@gmail.com>
2024-02-27 22:34:09 +0900

    update

--- .config/nvim/lsp.d/cmp.rc.lua
+++ .config/nvim/lsp.d/cmp.rc.lua
@@ -1,80 +1,75 @@
-local cmp = require 'cmp'
+local cmp = require("cmp")
 cmp.setup({
-  experimental = {
-    ghost_text = { hlgroup = "Comment" }
-  },
-  preselect = 'none',
-  completion = {
-    completeopt = 'menu,menuone,noinsert,noselect'
-  },
-  -- Enable LSP snippets
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-    end,
-  },
-  mapping = {
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    -- Add tab support
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<Tab>'] = cmp.mapping.select_next_item(),
-    ['<C-S-f>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<C-y>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = false,
-    })
-  },
-  -- Installed sources:
-  sources = {
-    { name = 'yank' },
-    { name = 'path' },                     -- file paths
-    { name = 'nvim_lsp' },                 -- from language server
-    { name = 'nvim_lsp_signature_help' },  -- display function signatures with current parameter emphasized
-    { name = 'nvim_lsp_document_symbol' }, -- display function signatures with current parameter emphasized
-    { name = 'emoji' },                    -- display function signatures with current parameter emphasized
-    { name = "codeium" },
-    { name = 'html-css' },
-    { name = 'crates',                  keyword_length = 2 },
-    { name = 'nvim_lua',                keyword_length = 2 }, -- complete neovim's Lua runtime API such vim.lsp.*
-    { name = 'vsnip' },                                       -- from language server
-    { name = 'buffer' },                                      -- source current buffer
-    -- { name = 'dictionary' },
-    { name = 'calc' },                                        -- source for math calculation
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  formatting = {
-    fields = { 'menu', 'abbr', 'kind' },
-    format = function(entry, item)
-      local menu_icon = {
-        nvim_lsp = 'λ',
-        -- vsnip = '⋗',
-        buffer = 'Ω',
-        path = '🖫',
-      }
-      item.menu = menu_icon[entry.source.name]
-      return item
-    end,
-  },
+	preselect = "none",
+	completion = {
+		completeopt = "menu,menuone,noinsert,noselect",
+	},
+	-- Enable LSP snippets
+	snippet = {
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+		end,
+	},
+	mapping = {
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		-- Add tab support
+		["<S-Tab>"] = cmp.mapping.select_prev_item(),
+		["<Tab>"] = cmp.mapping.select_next_item(),
+		["<C-S-f>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.close(),
+		["<C-y>"] = cmp.mapping.confirm({
+			behavior = cmp.ConfirmBehavior.Insert,
+			select = false,
+		}),
+	},
+	-- Installed sources:
+	sources = {
+		{ name = "yank" },
+		{ name = "path" }, -- file paths
+		{ name = "nvim_lsp" }, -- from language server
+		{ name = "nvim_lsp_signature_help" }, -- display function signatures with current parameter emphasized
+		{ name = "nvim_lsp_document_symbol" }, -- display function signatures with current parameter emphasized
+		{ name = "emoji" }, -- display function signatures with current parameter emphasized
+		{ name = "html-css" },
+		{ name = "crates", keyword_length = 2 },
+		{ name = "nvim_lua", keyword_length = 2 }, -- complete neovim's Lua runtime API such vim.lsp.*
+		{ name = "vsnip" }, -- from language server
+		{ name = "buffer" }, -- source current buffer
+		-- { name = 'dictionary' },
+		{ name = "calc" }, -- source for math calculation
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	formatting = {
+		fields = { "menu", "abbr", "kind" },
+		format = function(entry, item)
+			local menu_icon = {
+				nvim_lsp = "λ",
+				-- vsnip = '⋗',
+				buffer = "Ω",
+				path = "🖫",
+			}
+			item.menu = menu_icon[entry.source.name]
+			return item
+		end,
+	},
 })
-
 
 local dict = require("cmp_dictionary")
 
 dict.setup({
-  -- The following are default values.
-  exact_length = 2,
-  first_case_insensitive = false,
-  document = false,
-  document_command = "wn %s -over",
-  async = false,
-  sqlite = false,
-  max_number_items = -1,
-  debug = false,
+	-- The following are default values.
+	exact_length = 2,
+	first_case_insensitive = false,
+	document = false,
+	document_command = "wn %s -over",
+	async = false,
+	sqlite = false,
+	max_number_items = -1,
+	debug = false,
 })
--- .config/nvim/plugin.d/comment.rc.lua
+++ .config/nvim/plugin.d/comment.rc.lua
@@ -1,9 +1,13 @@
--- require('Comment').setup({
---   create_mappings = false,
---   pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
--- })
---
--- vim.keymap.set('n', '<C-_><C-_>', '<Plug>(comment_toggle_linewise_current)', { remap = true })
--- vim.keymap.set('n', '<C-/><C-/>', '<Plug>(comment_toggle_linewise_current)', { remap = true })
--- vim.keymap.set('v', '<C-_><C-_>', '<Plug>(comment_toggle_linewise_visual)', { remap = true })
--- vim.keymap.set('v', '<C-/><C-/>', '<Plug>(comment_toggle_linewise_visual)', { remap = true })
+vim.g.skip_ts_context_commentstring_module = true
+
+require("ts_context_commentstring").setup({})
+
+require("Comment").setup({
+	create_mappings = false,
+	pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+})
+
+vim.keymap.set("n", "<C-_><C-_>", "<Plug>(comment_toggle_linewise_current)", { remap = true })
+vim.keymap.set("n", "<C-/><C-/>", "<Plug>(comment_toggle_linewise_current)", { remap = true })
+vim.keymap.set("v", "<C-_><C-_>", "<Plug>(comment_toggle_linewise_visual)", { remap = true })
+vim.keymap.set("v", "<C-/><C-/>", "<Plug>(comment_toggle_linewise_visual)", { remap = true })
