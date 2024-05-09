20768a47d1abc8b522f0f2e044d8849746e8eb7b
kbwo <kabaaa1126@gmail.com>
2024-05-09 15:57:46 +0900

    update

---
+++ tampermonkey/gh-hide-whitespace.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         gh-hide-whitespace
+// @namespace    http://tampermonkey.net
+// @match        https://github.com/*/*/pull/*/files*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @description
+// @version      0.0.1
+// ==/UserScript==
+
+(() => {
+  "use strict";
+
+  const url = new URL(location.href);
+  const w = url.searchParams.get("w");
+  if (w === "1") {
+    return;
+  }
+  url.searchParams.set("w", "1");
+  location.href = url.href;
+})();
