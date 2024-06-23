0ba69de34841a04da2da6d97634716266eea3c64
kbwo <kabaaa1126@gmail.com>
2024-06-24 00:14:00 +0900

    update

--- .config/nvim/plugin.d/toggleterm.rc.lua
+++ .config/nvim/plugin.d/toggleterm.rc.lua
@@ -1,3 +1,10 @@
 require("toggleterm").setup{}
+local terminal_id = 1
+
+function open_new_terminal()
+  require("toggleterm.terminal").Terminal:new({ id = terminal_id }):toggle()
+  terminal_id = terminal_id + 1
+end
 
 vim.api.nvim_set_keymap('n', '<Leader>gt', ':ToggleTerm<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>gn", "<cmd>lua open_new_terminal()<CR>", { noremap = true, silent = true })
