7a28cfbefe16c60cdf1cd6abe2b7e8989139f5ff
kbwo <kabaaa1126@gmail.com>
2024-06-26 23:41:51 +0900

    update

--- .config/nvim/plugin.d/toggleterm.rc.lua
+++ .config/nvim/plugin.d/toggleterm.rc.lua
@@ -6,5 +6,5 @@ function open_new_terminal()
   terminal_id = terminal_id + 1
 end
 
-vim.api.nvim_set_keymap('n', '<Leader>gt', ':ToggleTerm<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>gt', ':ToggleTerm size=20<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>gn", "<cmd>lua open_new_terminal()<CR>", { noremap = true, silent = true })
--- .config/nvim/plugin.d/visual-multi.rc.vim
+++ .config/nvim/plugin.d/visual-multi.rc.vim
@@ -5,3 +5,4 @@ let g:VM_maps["Add Cursor Down"]    = '<M-j>'   "
 let g:VM_maps["Add Cursor Up"]      = '<c-u>'   "
 let g:VM_maps["Toggle Mappings"]    = '<CR>'    "
 let g:VM_maps["Exit"]               = '<c-[>' "
+let g:VM_set_statusline = 1
