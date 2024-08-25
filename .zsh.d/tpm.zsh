e40f378f1b1a67489916e839788d743d2ec1f2b3
kbwo <kabaaa1126@gmail.com>
2024-08-25 23:59:54 +0900

    update

--- .zsh.d/tpm.zsh
+++ .zsh.d/tpm.zsh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm && ~/.tmux/plugins/tpm/bin/install_plugins
 fi
