22b1fcb6c7b05c4853aff2a4583cadf33f759b78
kbwo <kabaaa1126@gmail.com>
2024-08-25 17:29:15 +0900

    update

--- ln.sh
+++ ln.sh
@@ -2,8 +2,9 @@
 mkdir -p ~/.config/nvim
 # ln -sf ~/dotfiles/.config/nvim/init.vim ~/.vimrc
 ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
-ln -sf ~/dotfiles/.zshrc ~/.zsh.d/.zshrc
+ln -sf ~/dotfiles/.zsh.d/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/.alacritty.arch.toml ~/.alacritty.toml
+# ln -sf ~/dotfiles/.alacritty.sway.toml ~/.alacritty.toml
+ln -sf ~/dotfiles/.alacritty.gnome.toml ~/.alacritty.toml
 ln -sf ~/dotfiles/.skhdrc ~/.skhdrc
 ln -sf ~/dotfiles/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
