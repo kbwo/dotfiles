54320eb72fdd4b504dbf9e87a2b7e570c1c5b127
kbwo <kabaaa1126@gmail.com>
2024-09-09 12:15:12 +0900

    refactor: update CopilotChat commit message prompt

--- .config/nvim/ftplugin/gitcommit.lua
+++ .config/nvim/ftplugin/gitcommit.lua
@@ -8,7 +8,9 @@ if has_copilot then
     end
     local chat = require("CopilotChat")
     chat.ask(
-    'Generate a concise and meaningful commit message title based on the changes made with conventional commits format. Make sure the title has maximum 50 characters. Do not put quotes around the title. Focus solely on creating the title, not the full commit message body.',
+    [[
+    Generate a concise and meaningful commit message title based on the changes made with conventional commits format. Make sure the title has maximum 50 characters. Do not put quotes around the title. Focus solely on creating the title, not the full commit message body.
+    ]],
       {
         selection = function(source)
           return require("CopilotChat.select").gitdiff(source, true)
