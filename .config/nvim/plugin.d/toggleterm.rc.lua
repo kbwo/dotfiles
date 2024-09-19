4a52d65480d6a1f94ce52d599ae4da6435fa3f5b
kbwo <kabaaa1126@gmail.com>
2024-09-19 22:47:45 +0900

    feat(nvim): add command to close all toggleterm buffers

--- .config/nvim/plugin.d/toggleterm.rc.lua
+++ .config/nvim/plugin.d/toggleterm.rc.lua
@@ -19,3 +19,28 @@ end
 
 vim.api.nvim_set_keymap('n', '<Leader>gt', '<cmd>lua toggle_or_new_terminal()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>gn", "<cmd>lua open_new_terminal()<CR>", { noremap = true, silent = true })
+
+-- Function to close all buffers with filetype 'toggleterm'
+local function close_all_toggleterm_buffers()
+  local buffers = vim.api.nvim_list_bufs()
+
+  for _, buf in ipairs(buffers) do
+    if vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_option(buf, 'filetype') == 'toggleterm' then
+      local windows = vim.fn.win_findbuf(buf)
+      if #windows > 0 then
+        for _, win in ipairs(windows) do
+          vim.api.nvim_win_close(win, true)
+        end
+      else
+        vim.api.nvim_buf_delete(buf, { force = true })
+      end
+    end
+  end
+end
+
+-- Define a Neovim command to close all toggleterm buffers
+vim.api.nvim_create_user_command(
+  'CloseToggletermBuffers',
+  close_all_toggleterm_buffers,
+  { desc = 'Close all toggleterm buffers' }
+)
