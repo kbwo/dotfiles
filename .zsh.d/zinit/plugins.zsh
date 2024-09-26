7394dde4e2648f58f4ec8f410475f188b7dacc6b
kbwo <kabaaa1126@gmail.com>
2024-09-26 21:03:28 +0900

    tiny fix zsh plugin

--- .zsh.d/zinit/plugins.zsh
+++ .zsh.d/zinit/plugins.zsh
@@ -1,11 +1,9 @@
-zinit load agkozak/zsh-z
-zinit light zsh-users/zsh-autosuggestions
 bindkey '^j' autosuggest-accept
 zinit wait lucid light-mode for \
+    'agkozak/zsh-z' \
     'zdharma-continuum/history-search-multi-word' \
     'zsh-users/zsh-syntax-highlighting' \
     'zsh-users/zsh-completions' \
     'migutw42/zsh-fzf-ghq' \
-    'zsh-users/zsh-autosuggestions' \
-    'jonmosco/kube-ps1'
+    'zsh-users/zsh-autosuggestions'
 autoload -Uz compinit && compinit
