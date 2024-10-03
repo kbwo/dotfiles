942c9bc9e4996d1142e242d3975bc40db7944900
kbwo <kabaaa1126@gmail.com>
2024-10-03 22:25:51 +0900

    feat: change filename format displayed by lualine

--- .config/nvim/plugin.d/lualine.rc.lua
+++ .config/nvim/plugin.d/lualine.rc.lua
@@ -1,3 +1,22 @@
+local function filename_with_parent()
+  -- Get the full path of the current file
+  local filepath = vim.fn.expand('%:p')
+
+  -- Get the filename
+  local filename = vim.fn.expand('%:t')
+
+  -- Get the parent directory's name
+  local parent_dir = vim.fn.fnamemodify(filepath, ':h:t')
+
+  -- Handle cases where the file is in the root directory
+  if parent_dir == '' then
+    return filename
+  else
+    return parent_dir .. '/' .. filename
+  end
+end
+
+
 require('lualine').setup {
   options = {
     icons_enabled = false,
@@ -23,7 +42,7 @@ require('lualine').setup {
         'filename',
         file_status = true,   -- Displays file status (readonly status, modified status)
         newfile_status = false, -- Display new file status (new file means no write after created)
-        path = 0,             -- 0: Just the filename
+        path = 1,             -- 0: Just the filename
         -- 1: Relative path
         -- 2: Absolute path
         -- 3: Absolute path, with tilde as the home directory
@@ -49,7 +68,25 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = { 'filename' },
+    lualine_c = { {
+        'filename',
+        file_status = true,   -- Displays file status (readonly status, modified status)
+        newfile_status = false, -- Display new file status (new file means no write after created)
+        path = 1,             -- 0: Just the filename
+        -- 1: Relative path
+        -- 2: Absolute path
+        -- 3: Absolute path, with tilde as the home directory
+        -- 4: Filename and parent dir, with tilde as the home directory
+
+        shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+        -- for other components. (terrible name, any suggestions?)
+        symbols = {
+          modified = '[+]',    -- Text to show when the file is modified.
+          readonly = '[-]',    -- Text to show when the file is non-modifiable or readonly.
+          unnamed = '[No Name]', -- Text to show for unnamed buffers.
+          newfile = '[New]',   -- Text to show for newly created file before first write
+        }
+      }},
     lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
