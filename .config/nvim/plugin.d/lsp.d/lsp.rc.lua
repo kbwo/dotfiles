0fefbe50e5d460287841d8c8b8ad04df3af60bae
kbwo <kabaaa1126@gmail.com>
2024-11-10 12:35:54 +0900

    feat: add testing-ls config

--- .config/nvim/plugin.d/lsp.d/lsp.rc.lua
+++ .config/nvim/plugin.d/lsp.d/lsp.rc.lua
@@ -7,7 +7,45 @@ require("fidget").setup {
   }
 }
 
-require("mason").setup()
+require("mason").setup({
+  ui = {
+    keymaps = {
+      ---@since 1.0.0
+      -- Keymap to expand a package
+      toggle_package_expand = "<CR>",
+      ---@since 1.0.0
+      -- Keymap to install the package under the current cursor position
+      install_package = "i",
+      ---@since 1.0.0
+      -- Keymap to reinstall/update the package under the current cursor position
+      update_package = "u",
+      ---@since 1.0.0
+      -- Keymap to check for new version for the package under the current cursor position
+      check_package_version = "c",
+      ---@since 1.0.0
+      -- Keymap to update all installed packages
+      update_all_packages = "U",
+      ---@since 1.0.0
+      -- Keymap to check which installed packages are outdated
+      check_outdated_packages = "C",
+      ---@since 1.0.0
+      -- Keymap to uninstall a package
+      uninstall_package = "X",
+      ---@since 1.0.0
+      -- Keymap to cancel a package installation
+      cancel_installation = "<C-c>",
+      ---@since 1.0.0
+      -- Keymap to apply language filter
+      apply_language_filter = "<C-u>",
+      ---@since 1.1.0
+      -- Keymap to toggle viewing package installation log
+      toggle_package_install_log = "<CR>",
+      ---@since 1.8.0
+      -- Keymap to toggle the help view
+      toggle_help = "g?",
+    }
+  }
+})
 local lspconfig = require('lspconfig')
 
 local mason_lspconfig = require("mason-lspconfig")
@@ -187,3 +225,24 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.api.nvim_buf_set_keymap(0, "n", "ccr", ":TSToolsRemoveUnusedImports<CR>", { noremap = true, silent = true })
   end,
 })
+
+
+local configs = require('lspconfig.configs')
+local util = require "lspconfig/util"
+
+configs.testing_ls = {
+  default_config = {
+    cmd = { "testing-language-server" },
+    filetypes = {},
+    root_dir = util.root_pattern(".testingls.toml", ".git" ),
+  },
+  docs = {
+    description = [[
+      https://github.com/kbwo/testing-language-server
+
+      Language Server for real-time testing.
+    ]],
+  },
+}
+
+lspconfig.testing_ls.setup{}
