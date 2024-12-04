49739ca4a47f85562a16ff78ebbe80e5d2c3b7c0
kbwo <kabaaa1126@gmail.com>
2024-12-04 18:39:23 +0900

    feat(zsh): add kb command

--- .zsh.d/misc.zsh
+++ .zsh.d/misc.zsh
@@ -1,3 +1,19 @@
- mkfile() { 
-  mkdir -p $( dirname "$1") && touch "$1" 
- }
+mkfile() {
+ mkdir -p $( dirname "$1") && touch "$1"
+}
+
+kb() {
+   timestamp=$(date +"%Y%m%d%H%M")
+   random=$(LC_ALL=C tr -dc 'a-z0-9' </dev/urandom | head -c6)
+
+
+   new_dir="${KBWO}/${timestamp}-${random}"
+
+   mkdir -p "$new_dir"
+
+   cd "$new_dir" || {
+       echo "Cannot move to the new directory: $new_dir"
+       return 1
+   }
+}
+
