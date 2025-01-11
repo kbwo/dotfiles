8f4e55370953a571729721b0f82baff9cb77864e
kbwo <kabaaa1126@gmail.com>
2025-01-11 18:06:04 +0900

    feat: update scripts

--- violentmonkey/gh-hide-whitespace.js
+++ violentmonkey/gh-hide-whitespace.js
@@ -4,19 +4,18 @@
 // @match        https://github.com/*/*/pull/*/files*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @description
+// @run-at       document-end
 // @version      0.0.1
 // ==/UserScript==
 
 (() => {
-  "use strict";
+    "use strict";
 
-  setInterval(() => {
     const url = new URL(location.href);
     const w = url.searchParams.get("w");
     if (w === "1") {
-      return;
+        return;
     }
     url.searchParams.set("w", "1");
     location.href = url.href;
-  }, 1000);
 })();
