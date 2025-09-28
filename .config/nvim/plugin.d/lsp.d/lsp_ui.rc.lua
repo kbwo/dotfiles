657c965c22e1d8272d82775780f1bd5929a7d12d
kbwo <kbwo.owbk@gmail.com>
2025-09-28 23:41:25 +0900

    update

--- .config/nvim/plugin.d/lsp.d/lsp_ui.rc.lua
+++ .config/nvim/plugin.d/lsp.d/lsp_ui.rc.lua
@@ -7,21 +7,7 @@ require("actions-preview").setup({
 
 vim.keymap.set({ "n" }, "cca", require("actions-preview").code_actions)
 
-require("hover").setup({
-	init = function()
-		require("hover.providers.lsp")
-	end,
-	preview_opts = {
-		border = "single",
-	},
-	preview_window = false,
-	title = true,
-	mouse_providers = {
-		"LSP",
-	},
-	mouse_delay = 1000,
-})
-vim.keymap.set("n", "csd", require("hover").hover, { desc = "hover.nvim" })
+vim.keymap.set("n", "csd", vim.lsp.buf.hover)
 vim.keymap.set("n", "<c-l>", vim.diagnostic.goto_next)
 vim.keymap.set("n", "<c-h>", vim.diagnostic.goto_prev)
 
@@ -59,38 +45,6 @@ _G.Rename = {
 }
 
 vim.keymap.set("n", "ccn", "<cmd>lua Rename.rename()<CR>, {silent = true}")
-
--- require("trouble").setup({
--- 	focus = true,
--- 	win = {
--- 		size = {
--- 			height = 25,
--- 		},
--- 	},
--- 	modes = {
--- 		split_diag = {
--- 			mode = "diagnostics",
--- 			preview = {
--- 				type = "split",
--- 				relative = "win",
--- 				position = "right",
--- 				size = 0.3,
--- 			},
--- 		},
--- 		diagnostics = {
--- 			groups = {
--- 				{ "filename", format = "{basename:Title} {count}" },
--- 			},
--- 		},
--- 	},
--- 	icons = {},
--- 	keys = {
--- 		["<Leader>ss"] = "jump_split",
--- 		["<Leader>vv"] = "jump_vsplit",
--- 	},
--- })
---
--- vim.keymap.set("n", "<leader>id", "<cmd>Trouble split_diag toggle<CR>, {silent = true}")
 
 require("fidget").setup({
 	notification = {
