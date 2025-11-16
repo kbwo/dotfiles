390eaa0dddc68c7843f1ff16f0d8b1445a11fddf
kbwo <kbwo.owbk@gmail.com>
2025-11-17 08:47:04 +0900

    update

--- .config/nvim/plugin.d/copilot.rc.lua
+++ .config/nvim/plugin.d/copilot.rc.lua
@@ -1,7 +1,14 @@
 vim.lsp.config("copilot", {
   cmd = {
     'mise', 'exec', 'node@22', '--', 'copilot-language-server', '--stdio'
-  }
+  },
+  filetypes = function(bufnr)
+    -- Disable for markdown files
+    if vim.bo[bufnr].filetype == "markdown" then
+      return false
+    end
+    return true
+  end
 })
 require("sidekick").setup({})
 vim.keymap.set("n", "<A-y>", require("sidekick").nes_jump_or_apply, { desc = "Accept Copilot NES suggestion", expr = true })
